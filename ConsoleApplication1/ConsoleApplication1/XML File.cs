2973d1f86acd599a59b698a7ccd3f2026b8fb039
valeria20 <sholomova96@gmail.com>
2017-02-16 12:16:27 +0300

    .

---
+++ ConsoleApplication1/ConsoleApplication1/XML File.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Linq;
+
+namespace ConsoleApplication1
+{
+    class XML_File
+    {
+
+        private XML_File() { }
+        public static XDocument LoadFile()
+        {
+            XDocument root = XDocument.Load("D:\\RD. HW - AT Lab#. 05 - Customers.xml");
+
+            return root;
+        }
+    }
+}
